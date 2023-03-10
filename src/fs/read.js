af839381a3c62d28bb2492ba0e769ca652950f36
CHAMOMILE <xsmokx228@gmail.com>
2023-03-10 13:47:35 +0300

    complete_first_task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,12 @@
+import fs from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    try{
+    const fileContent = await fs.readFile("src/fs/files/dontLookAtM.txt", "utf8");
+    console.log(fileContent);}
+    catch (err) {
+        console.error(err.message);
+      }
 };
 
 await read();
